1f0d5bce739d8540e80fe8a3f06199bf01093a8c
Kevin Julio <kevjuliow@gmail.com>
2025-01-16 23:37:20 +0700

    add footer

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-full h-[400px] bg-[#141416] absolute bottom-0'>
+      <div className='max-w-screen-xl text-white m-auto h-full flex flex-col'>
+        
+        <div className='w-[90%] mx-auto border-t-2 border-b-2 h-[70%] border-gray-400 my-10'>
+          asd
+        </div>
+        <div>
+
+        </div>
+
+
+      </div>
+    </div>
   )
 }
 
