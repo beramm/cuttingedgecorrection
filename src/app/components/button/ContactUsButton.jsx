d8224029669e6d35d790031f827ec1f6d5489420
stevanryan <stevanryan85@gmail.com>
2025-01-17 11:08:51 +0700

    add contact us button component

---
+++ src/app/components/button/ContactUsButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ArrowUpCircleSolid } from '../icon'
+import Link from 'next/link'
+
+const ContactUsButton = () => {
+  return (
+    <Link href={"/contact"} className="cursor-pointer hover:border-highlight duration-300 flex items-center justify-center gap-x-2 border-2 border-accent rounded-full p-1 pl-2">
+      <p>Contact Us</p>
+      <ArrowUpCircleSolid size={24} hexColor={"#E8E6DE"} />
+    </Link>
+  )
+}
+
+export default ContactUsButton
