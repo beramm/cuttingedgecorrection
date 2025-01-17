aa2120d4115a4562d0812b5a9971501bfe6a0d8f
stevanryan <stevanryan85@gmail.com>
2025-01-17 11:39:33 +0700

    change tailwind style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,14 @@
 export default function Home() {
   return (
     <div className="w-full h-[1200px]">
-  {/* Video Background */}
-  <video
-    className="absolute top-0 left-0 object-contain"
-    src="/videoplayback.webm"
-    autoPlay
-    loop
-    muted
-  ></video>
-
-</div>
-
+      {/* Video Background */}
+      <video
+        className="absolute top-0 left-0 right-0 object-contain block mx-auto"
+        src="/videoplayback.webm"
+        autoPlay
+        loop
+        muted
+      ></video>
+    </div>
   );
 }
