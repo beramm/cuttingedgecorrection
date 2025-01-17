eaaa1988dc413c0fabced6a26de7a2276e00c706
stevanryan <stevanryan85@gmail.com>
2025-01-17 11:44:29 +0700

    removing comment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <div className="w-full h-[1200px]">
-      {/* Video Background */}
       <video
         className="absolute top-0 left-0 right-0 object-contain block mx-auto"
         src="/videoplayback.webm"
