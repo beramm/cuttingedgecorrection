b74bf360178774a4a379b0bfaba2755197a6ad78
Kevin Julio <kevjuliow@gmail.com>
2025-01-17 13:05:54 +0700

    created caraosel component

---
+++ src/app/components/caraosels/ServicesCaraosel.jsx
@@ -0,0 +1,95 @@
+"use client";
+
+import React from "react";
+import { Navigation, Pagination, A11y, Parallax } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/parallax";
+import Image from "next/image";
+
+const ServicesCarousel = () => {
+  const services = [
+    {
+      title: "Ceramic Coatings",
+      description:
+        "Paint correction, superior UV & scratch resistance, and extremely hydrophobic.",
+      image: "/randomServices.webp",
+    },
+    {
+      title: "Swirl Removal",
+      description: "Eliminates scratches, brings back a smooth, glossy finish.",
+      image: "/randomServices.webp",
+    },
+    {
+      title: "Lorem Ipsum",
+      description:
+        "Detail service 3 ipsum dolor sit amet, consectetur adipiscing elit.",
+      image: "/randomServices.webp",
+    },
+    {
+      title: "Ceramic Coatings",
+      description:
+        "Paint correction, superior UV & scratch resistance, and extremely hydrophobic.",
+      image: "/randomServices.webp",
+    },
+    {
+      title: "Swirl Removal",
+      description: "Eliminates scratches, brings back a smooth, glossy finish.",
+      image: "/randomServices.webp",
+    },
+    {
+      title: "Lorem Ipsum",
+      description:
+        "Detail service 3 ipsum dolor sit amet, consectetur adipiscing elit.",
+      image: "/randomServices.webp",
+    },
+  ];
+
+  return (
+    <div className="w-full max-w-6xl mx-auto h-[500px] relative">
+      <Swiper
+        modules={[Navigation, Pagination, A11y, Parallax]}
+        spaceBetween={8}
+        slidesPerView={2.5}
+        centeredSlides={false}
+        navigation
+        pagination={{
+          clickable: true,
+          bulletActiveClass: "swiper-pagination-bullet-active bg-white",
+        }}
+        className="h-full w-full custom-swiper"
+      >
+        {services.map((service, index) => (
+          <SwiperSlide key={index} className="relative">
+            <div className="relative w-full h-full overflow-hidden">
+              {/* Parallax Image */}
+              <div
+                data-swiper-parallax="-300" // Parallax effect only on the image
+                className="relative w-full h-full cursor-pointer"
+              >
+                <Image
+                  src={service.image}
+                  alt={service.title}
+                  width={800}
+                  height={600}
+                  className="w-full h-full object-cover brightness-75 transition-transform duration-300 hover:scale-105"
+                />
+              </div>
+              {/* Content Box */}
+              <div className="absolute bottom-8 left-8 cursor-pointer bg-primary p-4 rounded-lg shadow-lg z-10 max-w-[280px] text-foreground border-t-highlight border-t-4 h-[130px]">
+                <h2 className="text-3xl font-bold mb-2">{service.title}</h2>
+                <p className="text-xs font-light">{service.description}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ServicesCarousel;
