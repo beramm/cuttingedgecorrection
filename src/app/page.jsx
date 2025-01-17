be6af8aab9bd456f640f587460bee0d76517e77d
Beramme <bramraysky232@gmail.com>
2025-01-17 18:58:02 +0700

    add second section on landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
             loop
             muted
           ></video>
-          <div className="h-[1200px] absolute top-0 left-0 flex flex-col items-start justify-center text-white pl-10">
+          <div className="h-[750px] absolute top-0 left-0 flex flex-col items-start justify-center text-white pl-10 ml-16">
             <h1 className="text-[60px] font-bold">CUTTING EDGE CORRECTION</h1>
             <div className="w-[550px] mb-8">
               <p>At CEC, we provide exceptional car detailing services right here in Geelong.
@@ -24,20 +24,41 @@ export default function Home() {
             <ContactUsButton />
           </div>
         </div>
-
       </div>
-     
+      <div className="w-full h-auto flex flex-col items-center mt-32">
+        <div className="w-full flex flex-row justify-between items-start px-10 mb-16">
+          <div className="w-1/2 pr-5">
+            <h2 className="text-[30px] font-bold">PASSION FOR CARS,<br />EXCELLENCE IN DETAILING.</h2>
+            <br />
+            <h2 className="text-[30px] font-bold">EXPERIENCE THE CUTTING EDGE</h2>
+          </div>
+          <div className="w-1/2 pl-5">
+            <p>
+              Delivering a 5-star finish is our standard, and we never settle for less.
+              Our unwavering dedication means your car's brilliance will stand the test of time.
+              <br /><br />
+              Want to see what all the hype's about?
+              Tap the button and let's chat.
+            </p>
+            <br />
+            <div className="w-fit">
+              <ContactUsButton />
+            </div>
+          </div>
+        </div>
+      </div>
+
 
       <div className="w-full  h-[1200px] mt-32">
         <div className="w-full text-center text-5xl font-extrabold mb-12">
-        <h1>Our <span className="bg-radial-gradient bg-clip-text text-transparent">Services</span>
-        </h1>
+          <h1>Our <span className="bg-radial-gradient bg-clip-text text-transparent">Services</span>
+          </h1>
         </div>
-        <ServicesCarousel/>
+        <ServicesCarousel />
       </div >
 
 
     </div>
-  
+
   );
 }
