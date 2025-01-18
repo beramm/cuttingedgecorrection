9beb607a252c33cb08ae33581e86a99d008049f6
stevanryan <stevanryan85@gmail.com>
2025-01-18 10:53:52 +0700

    add subscription plans page

---
+++ src/app/libs/subscription-plans-content.js
@@ -0,0 +1,40 @@
+export const subscriptionPlansContent = [
+  {
+    type: 'Basic',
+    price: 60,
+    bestValue: false,
+    content: [
+      '2 wash and waxes per month',
+      'thorough interior vacuuming',
+      'free air fresher with every wash'
+    ],
+    bgColor: 'bg-gradient-to-tr from-[#B34D9C] to-[#5F3E6B]',
+    highlightTextColor: 'text-[#B34D9C]'
+  },
+  {
+    type: 'Standard',
+    price: 120,
+    bestValue: false,
+    content: [
+      '4 wash and waxes per month',
+      '1 full interior detail per month',
+      'free air fresher with every wash'
+    ],
+    bgColor: 'bg-gradient-to-tr from-[#379CB7] to-[#3C5397]',
+    highlightTextColor: 'text-[#379CB7]'
+  },
+  {
+    type: 'Premium',
+    price: 200,
+    bestValue: true,
+    content: [
+      '4 wash and waxes per month',
+      '2 full interior details per month',
+      'priority scheduling',
+      'free air fresher with every wash',
+      '15% off for any additional services'
+    ],
+    bgColor: 'bg-gradient-to-tr from-[#DAA550] to-[#B53534]',
+    highlightTextColor: 'text-[#DAA550]'
+  },
+]
---
+++ src/app/subscription-plans/page.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { subscriptionPlansContent } from '../libs/subscription-plans-content'
+import SubscriptionPlansCard from '../components/card/subscription-plans-card/SubscriptionPlansCard'
+
+const SubscriptionPlans = () => {
+  return (
+    <div
+      style={{ backgroundImage: `url(carbon_background.png)`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+      className="h-full w-full px-8 md:px-4 xl:px-0 py-28"
+    >
+      <div className="relative w-full mt-12">
+        <div className="text-center text-5xl lg:text-[60spx] font-extrabold mb-12">
+          <h1>
+            CURATE YOUR{" "}
+            <span className="bg-radial-gradient bg-clip-text text-transparent">
+              EXPERIENCE
+            </span>
+          </h1>
+          <p className="text-sm font-light mt-8">
+            Introducing our exclusive subscription service!
+            <br/> Our subscription plans are designed to provide you with consistent, high-quality care for your vehicle, ensuring it always looks its best.
+            <br/> With convenient monthly payments and exclusive benefits, maintaining your car has never been easier.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row items-center justify-center gap-10">
+          {subscriptionPlansContent.map((eachPlan, index) => {
+            return (
+              <SubscriptionPlansCard key={index} data={eachPlan} />
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SubscriptionPlans
