86a1ebefa303f01060c034949fa2c0cf0770cbd3
stevanryan <stevanryan85@gmail.com>
2025-01-18 12:17:13 +0700

    add link to the logo and also change the style

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -5,13 +5,16 @@ import React, { useState } from 'react'
 import ContactUsButton from '../button/ContactUsButton'
 import NavbarLinks from './NavbarLinks'
 import { CloseIcon, HamburgerIcon } from '../icon'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <header className="w-full relative z-[999]">
+    <header className="w-full absolute z-[999]">
       <nav className="h-28 w-full max-w-screen-xl mx-auto">
         <div className="w-full h-full px-8 md:px-4 xl:px-0 flex items-center justify-between">
-          <Image src={"/cec_logo.png"} width={80} height={80} alt="CEC" />
+          <Link href={"/"} className="w-32">
+            <Image src={"/cec_logo.png"} width={80} height={80} alt="CEC" />
+          </Link>
           <div className="hidden md:block">
             <NavbarLinks />
           </div>
