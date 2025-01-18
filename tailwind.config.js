c9d646e76c19f052c8ca1d030a331769d7b076ae
Kevin Julio <kevjuliow@gmail.com>
2025-01-18 12:56:27 +0700

    tailwind material init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,6 +19,7 @@ module.exports = {
       },
     backgroundImage: {
       'radial-gradient': 'radial-gradient(circle, #00A2FF, #006199)',
+      'custom-gradient': 'linear-gradient(180deg, #141416 0%, #1B1B1E 50%, #1F1F22 100%)',
       },
     },
   },
