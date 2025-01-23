632d9bd25bcf2b745302aa46bc4f7f0f418371e2
stevanryan <stevanryan85@gmail.com>
2025-01-23 19:18:52 +0700

    change font and width size

--- src/app/components/button/ContactUsButton.jsx
+++ src/app/components/button/ContactUsButton.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const ContactUsButton = () => {
   return (
-    <Link href={"/contact"} className="w-32 cursor-pointer hover:border-highlight duration-300 flex items-center justify-center gap-x-2 border-2 border-accent rounded-full p-1 pl-2">
+    <Link href={"/contact"} className="w-40 text-xl cursor-pointer hover:border-highlight duration-300 flex items-center justify-center gap-x-2 border-2 border-accent rounded-full p-1 pl-2">
       <p>Contact Us</p>
       <ArrowUpCircleSolid size={24} hexColor={"#E8E6DE"} />
     </Link>
--- src/app/components/navbar/NavbarLinks.jsx
+++ src/app/components/navbar/NavbarLinks.jsx
@@ -14,14 +14,14 @@ const NavbarLinks = ({ handleOpenMobileNav }) => {
       <ul className="hidden md:flex items-center justify-center gap-x-8">
         {navbarMenu.map((eachMenu, index) => {
           return (
-            <li key={index} className="py-4 flex gap-x-1 items-center justify-center cursor-pointer group">
+            <li key={index} className="py-4 flex gap-x-1 items-center justify-center cursor-pointer group text-xl">
               {eachMenu.name === 'Subscription Plans' ? <Link href={"/subscription-plans"}>{eachMenu.name}</Link> : eachMenu.name}
               {eachMenu.showArrow && <span className="group-hover:transition-transform group-hover:rotate-180 duration-300 group-hover:duration-300">
                 <NavArrowDown size={24} hexColor={"#E8E6DE"} />
               </span>}
               {eachMenu.subMenu && 
               // <div className="absolute border border-green-500 w-full max-w-xl top-28 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 hidden group-hover:block transition-all duration-300">
-              <div className="absolute w-full max-w-xl top-48 left-1/2 transform -translate-x-1/2 -translate-y-28 z-50 opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity duration-300">
+              <div className="text-base absolute w-full max-w-xl top-48 left-1/2 transform -translate-x-1/2 -translate-y-28 z-50 opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity duration-300">
                 <ul className="w-full bg-opacity-90 p-2 bg-black grid grid-cols-2 row-auto text-center rounded-xl custom-box-shadow">
                   {eachMenu.submenuList.map((eachSubmenu, index) => (
                     <Link href={eachSubmenu.link} key={index} className="py-4">
