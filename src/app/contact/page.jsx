1454b15498f2ceac86168af48c88cfbb155376bc
stevanryan <stevanryan85@gmail.com>
2025-01-24 09:09:21 +0700

    change width to w-full

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -92,47 +92,47 @@ const ContactUsForm = () => {
 
             <div className="min-h-[900px] flex flex-col items-center justify-start gap-24 w-full gap-y-10 bg-custom-gradient px-8 md:px-4 xl:px-0 py-28">
             <div className="flex flex-col items-center mt-8">
-                <h1 className="font-bold text-[40px] text-center text-foreground">
+                <h1 className="font-black text-[40px] text-center text-foreground">
                     LOCK IN 
                     <span className="bg-radial-gradient bg-clip-text text-transparent">
                         &nbsp;YOUR DETAIL
                     </span>
                 </h1>
             </div>
-            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <form className="flex flex-col gap-4 w-full max-w-xl" onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="Full name *"
                     onChange={(e)=>setFullName(e.target.value)}
                     value={fullName}
-                    className="w-full sm:w-[400px] md:w-[500px] lg:w-[600px] border border-gray-500 rounded-lg p-3 text-white bg-transparent focus:outline-none focus:ring focus:ring-gray-700"
+                    className="w-full border border-gray-500 rounded-lg p-3 text-white bg-transparent focus:outline-none focus:ring focus:ring-gray-700"
                 />
                 <input
                     type="email"
                     placeholder="Email *"
                     onChange={(e)=>setEmail(e.target.value)}
                     value={email}
-                    className="w-full sm:w-[400px] md:w-[400px] lg:w-[600px] border border-gray-500 rounded-lg p-3 text-white bg-transparent focus:outline-none focus:ring focus:ring-gray-700"
+                    className="w-full border border-gray-500 rounded-lg p-3 text-white bg-transparent focus:outline-none focus:ring focus:ring-gray-700"
                 />
                 <input
                     type="tel"
                     placeholder="Phone number *"
                     onChange={(e)=>setPhone(e.target.value)}
                     value={phone}
-                    className="w-full sm:w-[400px] md:w-[400px] lg:w-[600px] border border-gray-500 rounded-lg p-3 text-white bg-transparent focus:outline-none focus:ring focus:ring-gray-700"
+                    className="w-full border border-gray-500 rounded-lg p-3 text-white bg-transparent focus:outline-none focus:ring focus:ring-gray-700"
                 />
                 <input
                     type="text"
                     placeholder="Type of service required"
                     onChange={(e)=>setService(e.target.value)}
                     value={service}
-                    className="w-full sm:w-[400px] md:w-[400px] lg:w-[600px] border border-gray-500 rounded-lg p-3 text-white bg-transparent focus:outline-none focus:ring focus:ring-gray-700"
+                    className="w-full border border-gray-500 rounded-lg p-3 text-white bg-transparent focus:outline-none focus:ring focus:ring-gray-700"
                 />
                 <textarea
                     placeholder="Please provide us any further information you think is important"
                     onChange={(e)=>setNotes(e.target.value)}
                     value={notes}
-                    className="w-full sm:w-[400px] md:w-[400px] lg:w-[600px] border border-gray-500 rounded-lg p-3 text-white bg-transparent focus:outline-none focus:ring focus:ring-gray-700"
+                    className="w-full border border-gray-500 rounded-lg p-3 text-white bg-transparent focus:outline-none focus:ring focus:ring-gray-700"
                     rows="5"
                 ></textarea>
                 <div className="w-full flex justify-end">
