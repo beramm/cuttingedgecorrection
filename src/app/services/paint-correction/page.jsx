6e0aec8b946738c5386c72e3d1a891011271fb45
Beramme <bramraysky232@gmail.com>
2025-01-29 21:41:35 +0700

    fix metadata,title, changing email

--- src/app/services/paint-correction/page.jsx
+++ src/app/services/paint-correction/page.jsx
@@ -6,7 +6,7 @@ import { paintCorrectionResults } from '../../libs/service-results-carousel'
 const PaintCorrection = () => {
   return (
     <>
-      <title>Ceramic Coating - Cutting Edge Correction</title>
+      <title>Paint Correction - Cutting Edge Correction</title>
       <meta name="description" content="Paint Correction: Paint Correction restores your vehicle’s paintwork to its original brilliance by removing scratches, swirl marks, and oxidation. This process reveals unmatched clarity and depth, enhancing both the appearance and long-term value of your car. Transform your vehicle with a shine that demands respect, achieving results like a freshly painted ride without the high cost." />
       <EachServicePage service={paintCorrection} serviceName="paint-correction" />
     </>
