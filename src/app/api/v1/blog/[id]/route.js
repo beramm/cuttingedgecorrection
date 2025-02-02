c440c0bbdaf8c0460a4f0a8da37c6a11e292c724
Kevin Julio <kevjuliow@gmail.com>
2025-02-02 10:55:06 +0700

    API endpoint for blog

---
+++ src/app/api/v1/blog/[id]/route.js
@@ -0,0 +1,75 @@
+import prisma from "../../../../libs/PrismaClient/prisma";
+import { NextResponse } from "next/server";
+
+export const DELETE = async (req, { params }) => {
+  try {
+    const id = parseInt(params.id);
+
+    const existingBlog = await prisma.blog.findUnique({
+      where: { id: id }
+    });
+
+    if (!existingBlog) {
+      return NextResponse.json(
+        { status: 404, error: "Blog not found" },
+        { status: 404 }
+      );
+    }
+
+    await prisma.blog.delete({
+      where: { id: id }
+    });
+
+    return NextResponse.json({
+      status: 200,
+      message: "Blog successfully deleted"
+    });
+
+  } catch (error) {
+    console.error("Error in Deleting Blog:", error);
+    return NextResponse.json(
+      { status: 500, error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+};
+
+export const PATCH = async (req, { params }) => {
+  try {
+    const id = parseInt(params.id);
+    const body = await req.json();
+
+    const existingBlog = await prisma.blog.findUnique({
+      where: { id: id },
+    });
+
+    if (!existingBlog) {
+      return NextResponse.json(
+        { status: 404, error: "Blog not found" },
+        { status: 404 }
+      );
+    }
+
+    const updatedBlog = await prisma.blog.update({
+      where: { id: id },
+      data: {
+        slug: body.slug || existingBlog.slug,
+        title: body.title || existingBlog.title,
+        content: body.content || existingBlog.content,
+      },
+    });
+
+    return NextResponse.json({
+      status: 200,
+      message: "Blog successfully updated",
+      blog: updatedBlog,
+    });
+
+  } catch (error) {
+    console.error("Error in Updating Blog:", error);
+    return NextResponse.json(
+      { status: 500, error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+};
---
+++ src/app/api/v1/blog/route.js
@@ -0,0 +1,35 @@
+import prisma from "../../../libs/PrismaClient/prisma";
+import { NextResponse } from "next/server"
+
+export const GET = async() => {
+  const blog = await prisma.blog.findMany({
+    orderBy: { created_at: 'desc' }
+  })
+
+  return NextResponse.json({ status: 200, data: blog })
+}
+
+
+export const POST = async (req)=> { 
+  try{
+  const body = await req.json();
+  const { slug , title , content } = body
+
+  const blog = await prisma.blog.create({
+    data: {
+     slug ,
+     title,
+     content
+    },
+  });
+
+  return NextResponse.json({
+    status: 201,
+    message: "Successfully import blog",
+    blog,
+  });
+} catch (error) {
+  console.error("Error in Importing blog :", error);
+  return NextResponse.json({ status: 500, error: "Internal server error" } , {status : 500});
+}
+}
