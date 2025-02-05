e646bbe069b7cf8b037ea62abf3ebc02f32defd6
Beramme <bramraysky232@gmail.com>
2025-02-05 17:30:22 +0700

    fix layout

--- src/app/api/v1/blog/route.js
+++ src/app/api/v1/blog/route.js
@@ -7,7 +7,7 @@ export const POST = async (req) => {
   try {
     // Get form data from request
     const formData = await req.formData();
-    
+
     const title = formData.get("title");
     const content = formData.get("content");
     const slug = formData.get("slug");
@@ -57,12 +57,6 @@ export const POST = async (req) => {
   }
 };
 
-export const config = {
-  api: {
-    bodyParser: false, // Required for handling file uploads
-  },
-};
-
 export const GET = async (req) => {
   try {
     const { searchParams } = new URL(req.url);
@@ -94,4 +88,10 @@ export const GET = async (req) => {
       { status: 500 }
     );
   }
+};
+
+export const config = {
+  api: {
+    bodyParser: false, // Required for handling file uploads
+  },
 };
