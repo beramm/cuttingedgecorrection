0b525cea6d20c3bbda41b97201a181ab692e3799
Beramme <bramraysky232@gmail.com>
2025-02-05 21:07:02 +0700

    route.js

--- src/app/components/icon/index.js
+++ src/app/components/icon/index.js
@@ -174,13 +174,7 @@ export const TrashSolidIcon = ({ size, hexColor }) => {
   )
 }
 
-export const CartIcon = ({ size, hexColor }) => (
-  <svg width={size} height={size} strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color={hexColor}>
-    <path d="M3 6H22L19 16H6L3 6ZM3 6L2.25 3.5" stroke={hexColor} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-    <path d="M11 19.5C11 20.3284 10.3284 21 9.5 21C8.67157 21 8 20.3284 8 19.5" stroke={hexColor} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-    <path d="M17 19.5C17 20.3284 16.3284 21 15.5 21C14.6716 21 14 20.3284 14 19.5" stroke={hexColor} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-  </svg>
-)
+
 
 export const CartRemoveIcon = ({ size, hexColor }) => (
   <svg width={size} height={size} strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color={hexColor}>
