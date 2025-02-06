fce40b269a87f33f8ad7bdff494303b69790d995
Kevin Julio <kevjuliow@gmail.com>
2025-02-06 15:22:11 +0700

    loading spinner in detailing guides

--- src/app/detailing-guides/[slug]/page.jsx
+++ src/app/detailing-guides/[slug]/page.jsx
@@ -3,10 +3,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Alert, Button } from "@material-tailwind/react";
+import { LoadingSpinner } from '../../components/icon';
 
 
 const SingleBlog = ({ params }) => {
     const [guide, setGuide] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
     const slug = params.slug
     const [showAlert, setShowAlert] = useState(false);
     const [alertMessage, setAlertMessage] = useState("");
@@ -14,6 +16,7 @@ const SingleBlog = ({ params }) => {
 
     useEffect(() => {
         const fetchResults = async () => {
+            setIsLoading(true);
             try {
                 const response = await axios.get(`/api/v1/blog/${slug}`)
                 setGuide(response.data.blog)
@@ -22,10 +25,19 @@ const SingleBlog = ({ params }) => {
                 setAlertType("error");
                 setAlertMessage("Error fetching blog. Please try again.");
                 setShowAlert(true);
+            } finally {
+                setIsLoading(false);
             }
         }
         fetchResults()
     }, [])
+
+    if (isLoading) {
+        return (<div className='w-full h-[100vh] flex items-center justify-center'>
+        <LoadingSpinner/>;
+        </div>
+        )
+    }
 
     return (
         <>
@@ -38,7 +50,7 @@ const SingleBlog = ({ params }) => {
             <div
                 className="h-full w-full px-8 md:px-12 xl:px-0 pt-28 pb-8"
             >
-                <div  className="relative w-full mt-12">
+                <div className="relative w-full mt-12">
                     <div className={`max-w-6xl mx-auto`}>
                         {/* Thumbnail */}
                         {guide?.thumbnail && (
@@ -61,13 +73,9 @@ const SingleBlog = ({ params }) => {
                                 </>
                             )}
                         </p>
-                        <h1 className="text-5xl font-bold mb-8">{guide ? guide.title : "Loading..."}</h1>
-
-
+                        <h1 className="text-5xl font-bold mb-8">{guide.title}</h1>
                         <p className="text-lg text-white-700" dangerouslySetInnerHTML={{ __html: guide.content }} />
                     </div>
-
-
 
                     {showAlert && (
                         <Alert
@@ -97,8 +105,6 @@ const SingleBlog = ({ params }) => {
                 </div>
             </div>
         </>
-
-
     );
 }
 
