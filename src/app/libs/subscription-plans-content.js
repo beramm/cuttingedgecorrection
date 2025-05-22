0a367a5f3c3cbeaf9316c221c3aa3b3b0f17285a
beramm <bramraysky232@gmail.com>
2025-05-22 13:50:49 +0700

    change content on subscription premium card

--- src/app/libs/subscription-plans-content.js
+++ src/app/libs/subscription-plans-content.js
@@ -29,7 +29,7 @@ export const subscriptionPlansContent = [
     bestValue: true,
     content: [
       '4 wash and waxes per month',
-      '2 full interior details per month',
+      '1 full interior details per month',
       'priority scheduling',
       'free air fresher with every wash',
       '15% off for any additional services'
