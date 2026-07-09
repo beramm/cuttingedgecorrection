c5341d1fa3da4b8681a56edccfb70599ccfc4b62
beramm <bramraysky232@gmail.com>
2026-07-09 13:22:12 +0800

    feat: updating location for new site

--- src/app/our-location/page.jsx
+++ src/app/our-location/page.jsx
@@ -14,7 +14,7 @@ const OurLocation = () => {
       <title>Our Location - Cutting Edge Correction</title>
       <meta
         name="description"
-        content="Visit Cutting Edge Correction at 67 Walsgott Street, North Geelong, VIC 3215, where the magic happens. We are open 7 days a week to serve you. Contact us at 0450 649 257 or email info@cecdetailing.com.au for inquiries. "
+        content="Visit Cutting Edge Correction at 9/118 Bellarine Highway, Newcomb, where the magic happens. We are open 7 days a week to serve you. Contact us at 0450 649 257 or email info@cecdetailing.com.au for inquiries. "
       />
       <div
         style={{
@@ -58,12 +58,12 @@ const OurLocation = () => {
                   </div>
                   <p className="hover:underline text-sm font-light mt-2">
                     <a
-                      href="https://www.google.com/maps/place/Cutting+Edge+Correction/@-38.1167031,144.3417704,17z/data=!3m1!4b1!4m6!3m5!1s0x6ad4118fa7bfd889:0x19eba5f83ed455b3!8m2!3d-38.1167031!4d144.3443453!16s%2Fg%2F11pwphknv6?entry=ttu&g_ep=EgoyMDI1MDExNS4wIKXMDSoASAFQAw%3D%3D"
+                      href = "https://www.google.com/maps/place/Cutting+Edge+Correction/@-38.1733169,144.3966248,18.94z/data=!4m6!3m5!1s0x6ad4118fa7bfd889:0x19eba5f83ed455b3!8m2!3d-38.1735363!4d144.3972814!16s%2Fg%2F11pwphknv6?entry=ttu&g_ep=EgoyMDI2MDcwNi4wIKXMDSoASAFQAw%3D%3D"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="hover:underline text-sm font-light"
                     >
-                      67 Walsgott Street, North Geelong, VIC 3215
+                      9/118 Bellarine Highway, Newcomb
                     </a>
                   </p>
                 </div>
